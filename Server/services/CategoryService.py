32d16c01f2ce07765a7cd9001c10a4def90e6aff
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-05-03 17:53:22 -0300

    Changes on response builder naming

--- Server/services/CategoryService.py
+++ Server/services/CategoryService.py
@@ -1,5 +1,4 @@
-from utils.response import build_response
-
+from utils import response_api
 
 class CategoryService:
   def __init__(self, category_repository):
@@ -9,32 +8,32 @@ class CategoryService:
     response = self.repository.get_by_id(id)
 
     if response:
-      return build_response(200, "Categoria encontrada!", response)
+      return response_api.build(200, "Categoria encontrada!", response)
     else:
-      return build_response(200, "Nenhuma categoria encontrada.")
+      return response_api.build(200, "Nenhuma categoria encontrada.")
 
   def list_categories(self):
     response = self.repository.get_all(id)
 
     if len(categories) == 0:
-      return build_response(200, "Nenhuma categoria encontrada.")
+      return response_api.build(200, "Nenhuma categoria encontrada.")
     else:
-      return build_response(200, "Categorias encontradas com sucesso.", categories)
+      return response_api.build(200, "Categorias encontradas com sucesso.", categories)
 
   def add_category(self, data):
     # Nome
     name = data.get("name")
 
     if not name or name.strip() == '':
-       return build_response(400, "É necessário informar um nome para a categoria.")
+       return response_api.build(400, "É necessário informar um nome para a categoria.")
 
     existing_category = None # Lógica de busca
 
     if existing_category:
-      return build_response(400, "Já existe uma categoria com o nome informado.")
+      return response_api.build(400, "Já existe uma categoria com o nome informado.")
 
     if len(name) > 20:
-      return build_response(400, "O nome da categoria pode ter no maximo 20 caracteres.")
+      return response_api.build(400, "O nome da categoria pode ter no maximo 20 caracteres.")
 
     # Pode conter caracteres não alfabéticos? sim&nao! talvez? >.<
     # Nome pode conter mais de uma palavra? nao vejo por que nao  poderia t er  es pa  co
@@ -43,20 +42,20 @@ class CategoryService:
     description = data.get("description")
 
     if not description or description.strip() == '':
-      return build_response(400, "É necessário informar uma descrição para a categoria.")
+      return response_api.build(400, "É necessário informar uma descrição para a categoria.")
 
     # Remover?
     if len(description) > 500:
-      return build_response(400, "A descrição deve conter pelo menos 10 caracteres.")
+      return response_api.build(400, "A descrição deve conter pelo menos 10 caracteres.")
 
     # Ícone
     icon = data.get("icon")
 
     if not icon or icon.strip() == '':
-       return build_response(400, "É necessário informar um ícone para a categoria.")
+       return response_api.build(400, "É necessário informar um ícone para a categoria.")
 
     if len(icon) != 3:
-       return build_response(400, "O ícone deve ter exatamente 3 caracteres.")
+       return response_api.build(400, "O ícone deve ter exatamente 3 caracteres.")
 
     # Ver lógica dos moderadores
 
@@ -69,9 +68,9 @@ class CategoryService:
 
     response = self.repository.new_entry(new_category)
     if response:
-      return build_response(200, "Categoria criada com sucesso.", response)
+      return response_api.build(200, "Categoria criada com sucesso.", response)
     else:
-      return build_response(500, "Houve um erro ao criar a categoria.")
+      return response_api.build(500, "Houve um erro ao criar a categoria.")
 
   def update_category(self, id, data):
     category = self.repository.get_by_id(id)
@@ -82,20 +81,20 @@ class CategoryService:
     response = self.repository.update_on_id(id)
 
     if response:
-      return build_response(200, "Categoria atualizada com sucesso.", response)
+      return response_api.build(200, "Categoria atualizada com sucesso.", response)
     else:
-      return build_response(500, "Houve um erro ao atualizar a categoria.")
+      return response_api.build(500, "Houve um erro ao atualizar a categoria.")
 
   def delete_category(self, id):
     category = self.repository.get_by_id(id)
     response = False
 
     if not category:
-      return build_response(400, "Categoria não encontrada.")
+      return response_api.build(400, "Categoria não encontrada.")
 
     response = self.repository.delete_on_id(id)
 
     if response:
-      return build_response(200, "Categoria deletada com sucesso.")
+      return response_api.build(200, "Categoria deletada com sucesso.")
     else:
-      return build_response(500, "Houve um erro ao deletar a categoria.")
+      return Response(500, "Houve um erro ao deletar a categoria.")
