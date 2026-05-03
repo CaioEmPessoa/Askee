2d3f227a22f227a89f2789b0c09b56f5716c872d
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-05-03 17:14:16 -0300

    Added custom new_entry method for post wich adds current data automatically

--- Server/repositories/Post.py
+++ Server/repositories/Post.py
@@ -1,5 +1,11 @@
+import time
 from .RegisterBase import RegisterBase
 
 class Post(RegisterBase):
     def __init__(self):
         super().__init__("Posts")
+
+    def new_entry(self, info):
+        info['data'] =  time.strftime("%Y-%m-%d %H:%M", time.gmtime())
+
+        return self.__append_info(info)
