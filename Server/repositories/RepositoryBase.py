b75fa73e9792b362332a7abf25fd6df2ef265ae2
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-05-04 17:56:43 -0300

    Changed static methods from __ to _

--- Server/repositories/RepositoryBase.py
+++ Server/repositories/RepositoryBase.py
@@ -2,84 +2,84 @@ import json
 import uuid
 from os import getcwd
 
-class RegisterBase():
+class RepositoryBase():
     def __init__(self, db_register_name):
-        self.__register_name = db_register_name
-        self.__register_location = f"{getcwd()}/../Database/{self.__register_name}.json"
+        self._register_name = db_register_name
+        self._register_location = f"{getcwd()}/../Database/{self._register_name}.json"
 
         self.cache = None
-        self.__build_cache()
+        self._build_cache()
 
     ## ===== HELPER FUNCTIONS =====
-    def __read_file (self):
+    def _read_file (self):
         result = ''
-        with open(self.__register_location, "r") as r:
+        with open(self._register_location, "r") as r:
             result = json.load(r)
 
         return result
 
-    def __write_file(self, info):
-        with open(self.__register_location, "w") as w:
+    def _write_file(self, info):
+        with open(self._register_location, "w") as w:
             json.dump(info, w, indent=4)
 
-        self.__build_cache()
+        self._build_cache()
 
-    def __write_cache(self):
+    def _write_cache(self):
         converted_values = list(self.cache.values())
-        self.__write_file(converted_values)
+        self._write_file(converted_values)
 
-    def __build_cache(self):
-        self.cache = {item['id']: item for item in self.__read_file()}
+    def _build_cache(self):
+        self.cache = {item['id']: item for item in self._read_file()}
 
-    def __gen_id(self):
+    def _gen_id(self):
         # try: new_id = max(self.cache)+1
         # except ValueError: new_id = 1
-        # except TypeError: raise Exception(f"{self.__register_name} possui um registro inválido!")
+        # except TypeError: raise Exception(f"{self._register_name} possui um registro inválido!")
         return uuid.uuid4().hex
 
-    def __update_on_id(self, id, info):
+    def _update_on_id(self, id, info):
         if id not in self.cache:
             return False
 
         info['id'] = id
 
         self.cache[id] = info
-        self.__write_cache()
+        self._write_cache()
 
         return self.cache[id]
 
-    def __append_info(self, info):
+    def _append_info(self, info):
         if 'id' in info:
             return False
 
-        new_id = self.__gen_id()
+        new_id = self._gen_id()
 
         info['id'] = new_id
 
         self.cache[new_id] = info
-        self.__write_cache()
+        self._write_cache()
 
         return self.cache[new_id]
 
-    def __delete_on_id(self, id):
+    def _delete_on_id(self, id):
         if id not in self.cache or id == None:
             return False
 
         self.cache.pop(id)
-        self.__write_cache()
+        self._write_cache()
 
         return True
 
     ## ===== INSERT FUNCTIONS =====
     def new_entry(self, info):
-        return self.__append_info(info)
+        return self._append_info(info)
 
     def update_on_id(self, id, info):
         updt_info = self.cache[id] | info
-        return self.__update_on_id(id, updt_info)
+        return self._update_on_id(id, updt_info)
 
     def delete_on_id(self, id):
-        return self.__delete_on_id(id)
+        return self._delete_on_id(id)
 
     ## ===== QUERY FUNCTIONS =====
     def get_all(self):
