d27af23fe45f5d448d25200fa4ed50e1a7168693
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-05-05 16:53:36 -0300

    Removed auth from posts and comments

--- Server/controllers/CommentController.py
+++ Server/controllers/CommentController.py
@@ -7,7 +7,6 @@ comment_service = CommentService(comment_repository)
 comment_bp = Blueprint('comment', __name__)
 
 @comment_bp.route('/comments', methods=['POST'])
-@require_auth
 def new_comments():
     data = request.json
     data["user_id"] = g.user_id
@@ -16,19 +15,16 @@ def new_comments():
     return jsonify(comment), comment["status"]
 
 @comment_bp.route('/comments', methods=['GET'])
-@require_auth
 def list_allComments():
     comment = comment_service.list_comments()
     return jsonify(comment), comment["status"]
 
 @comment_bp.route('/comments/<id>', methods=['GET'])
-@require_auth
 def list_comments(id):
     comment = comment_service.list_comment(id)
     return jsonify(comment), comment["status"]
 
 @comment_bp.route('/comments/<id>', methods=['DELETE'])
-@require_auth
 def delete_comments(id):
     comment = comment_service.delete_comment(id)
     return jsonify(comment), comment["status"]
--- Server/controllers/PostController.py
+++ Server/controllers/PostController.py
@@ -8,7 +8,6 @@ post_service = PostService(postRepository)
 post_bp = Blueprint('post', __name__)
 
 @post_bp.route('/post', methods=['POST'])
-@require_auth
 def new_post():
     data = request.json
     data["user_id"] = g.user_id
@@ -17,20 +16,17 @@ def new_post():
     return jsonify(post), post["status"]
 
 @post_bp.route('/post', methods=['GET'])
-@require_auth
 def list_posts():
     post = post_service.list_posts()
     #Os status HTTP estão vindo direto do service
     return jsonify(post), post["status"]
 
 @post_bp.route('/post/<id>', methods=['GET'])
-@require_auth
 def list_post(id):
     post = post_service.list_post(id)
     return jsonify(post), post["status"]
 
 @post_bp.route('/post/<id>', methods=['DELETE'])
-@require_auth
 def delete_post(id):
     post = post_service.delete_post(id)
     return jsonify(post), post["status"]
