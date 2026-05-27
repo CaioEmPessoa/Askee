959ebca678bf2f572984c52b7e39c419b6ae5a73
CaioEmPessoa <caiopolonioabruno@gmail.com>
2026-05-27 10:47:51 -0300

    Sign-up feito

--- Public/modules/cadastro.py
+++ Public/modules/cadastro.py
@@ -8,8 +8,47 @@ from PyQt5.QtPrintSupport import *
 from windows.cadastro import Ui_jan_cadastro
 import os,sys
 
+from AskeeRequests import *
+from Interatcion.configs import Configs
+
 class cadastro(QDialog):
     def __init__(self,*args,**argvs):
         super(cadastro,self).__init__(*args,**argvs)
+        self.configs = Configs()
+        self.authRequests = Auth()
         self.ui = Ui_jan_cadastro()
         self.ui.setupUi(self)
+
+        self.ui.cad_butt_enter.clicked.connect(self.cadastro)
+
+    def cadastro(self):
+        print("cadastrando...")
+        payload = {
+            "name": self.ui.cad_line_login.text(),
+            "email": self.ui.cad_line_login.text(),
+            "username": self.ui.cad_line_login.text(),
+            "password": self.ui.cad_line_passwd.text(),
+            "icon": self.ui.cad_box_icon.currentText(),
+            "about": self.ui.cad_txt_bio.toPlainText(),
+            "is_super": False,
+            "is_moderator": False
+        }
+        cadastro_response = self.authRequests.signup(payload)
+
+        if cadastro_response.httpCode == 200:
+            print("cadastrado!")
+            self.configs.current_user = cadastro_response.jsonResponse.get('data')
+
+            err_msg = QMessageBox()
+            err_msg.setIcon(QMessageBox.Icon.Information)
+            err_msg.setWindowTitle("Cadastrado com sucesso!")
+            err_msg.setText("Por favor, volte a tela incial e realize seu login agora.")
+            err_msg.exec()
+
+            self.close()
+        else:
+            err_msg = QMessageBox()
+            err_msg.setIcon(QMessageBox.Icon.Warning)
+            err_msg.setWindowTitle("Erro ao cadastrar!")
+            err_msg.setText(cadastro_response.jsonResponse.get('message'))
+            err_msg.exec()
